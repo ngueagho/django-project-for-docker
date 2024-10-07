86bfb9330087c42d89539edf018c33ba2c7f5755
ROBERTO <103413594+ngueagho@users.noreply.github.com>
2024-10-07 12:01:33 +0100

    Update settings.py

--- robertoproject/settings.py
+++ robertoproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-el$*disl*m&lwqwd+c+pafsr_#=8e9@9++yr*tq1yo!rp@!4+^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
